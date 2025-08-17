2e13874fc2c5b283fbb7b3440156ea625fc21acb
ravindutw <ravindu@ravinduw.com>
2025-08-17 18:19:11 +0530

    Initial commit

---
+++ src/main/java/com/ravinduw/demo/springwebexample1/SpringWebExample1Application.java
@@ -0,0 +1,13 @@
+package com.ravinduw.demo.springwebexample1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWebExample1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringWebExample1Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/ravinduw/demo/springwebexample1/SpringWebExample1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ravinduw.demo.springwebexample1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWebExample1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
